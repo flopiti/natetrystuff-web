b2007ff7ba4649a709db286df51d0c11327a274d
Nathan <npieraut@live.fr>
2024-05-18 01:10:24 -0400

    dynamic

--- natetrystuff-web/app/api/add-request/route.ts
+++ natetrystuff-web/app/api/add-request/route.ts
@@ -1,13 +1,12 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
-    const baseURL = process.env.NODE_ENV === 'production' ? process.env.CODE_HELPER_URL : 'http://your-production-url.com';
-    console.log(baseURL); // For debugging, remove in production
-    const res = await fetch(`${baseURL}/add-request`, {
+    console.log(process.env.CODE_HELPER_URL)
+    const res = await fetch(`${process.env.CODE_HELPER_URL}/add-request`, {
         headers: {
           'Content-Type': 'application/json',
         },
-    });  
+    });
 
     const data = await res.json();
     return new NextResponse(JSON.stringify({ data }), {
@@ -17,3 +16,5 @@ export async function GET(request: NextRequest) {
         },
     });
 }
+
+export const dynamic = 'auto'
