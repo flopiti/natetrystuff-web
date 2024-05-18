e5d9ccf218d0f2e4758f098940e99ccaddc6f833
Nathan <npieraut@live.fr>
2024-05-18 01:17:08 -0400

    no need for it

--- natetrystuff-web/app/code.tsx
+++ natetrystuff-web/app/code.tsx
@@ -2,15 +2,7 @@ import { use, useState } from "react";
 
 const Code = () => {
     const[newResourceName, setNewResourceName] = useState('');
-    const addGetOneToMeal = async () => {
-        const response = await fetch('/api/add-request', {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        });
-        const data = await response.json();
-    }
+
 
     const createNewResource = async () => {
         const response = await fetch(`/api/create-new-resource/${newResourceName}`, {
@@ -43,7 +35,6 @@ const Code = () => {
 
     return (
         <div className="bg-yellow-500 flex-grow w-full p-4 m-24">
-            <button onClick={()=>addGetOneToMeal()}>Add Get One to Meals</button>
             <input type="text" className="text-black" placeholder="Enter resource name" value={newResourceName} onChange={(e:any)=>setNewResourceName(e.target.value)} />
             <input type="text" className="text-black"  placeholder="Enter property1" value={property1Name} onChange={(e:any)=>setNewProperty1Name(e.target.value)} />
             <button onClick={()=>createNewResource()}>
