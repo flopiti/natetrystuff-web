001f770b9f2b0c7bcd707fe49f4b369549ec77b9
Nathan <npieraut@live.fr>
2024-05-18 01:04:33 -0400

    test

--- natetrystuff-web/app/api/add-request/route.ts
+++ natetrystuff-web/app/api/add-request/route.ts
@@ -1,12 +1,13 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
-    console.log(process.env.CODE_HELPER_URL)
-    const res = await fetch(`${process.env.CODE_HELPER_URL}/add-request`, {
+    const baseURL = process.env.NODE_ENV === 'production' ? process.env.CODE_HELPER_URL : 'http://your-production-url.com';
+    console.log(baseURL); // For debugging, remove in production
+    const res = await fetch(`${baseURL}/add-request`, {
         headers: {
           'Content-Type': 'application/json',
         },
-    });
+    });  
 
     const data = await res.json();
     return new NextResponse(JSON.stringify({ data }), {
