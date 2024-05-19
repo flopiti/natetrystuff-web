5c35d2348287d1a06084dfc249aa8d6950443882
Nathan <npieraut@live.fr>
2024-05-19 19:05:01 -0400

    code central

---
+++ natetrystuff-web/app/code-central.tsx
@@ -0,0 +1,10 @@
+const CodeCentral = () => {
+    return (
+        <div>
+        <h1>Code Central</h1>
+        <p>Check out my code snippets and projects!</p>
+        </div>
+    );
+    }
+
+export default CodeCentral;
