1b9deb2ae0b37c0ad601525a38278a292bcdf8f7
Nathan <npieraut@live.fr>
2024-05-20 10:51:15 -0400

    sections

--- natetrystuff-web/app/code-central.tsx
+++ natetrystuff-web/app/code-central.tsx
@@ -21,7 +21,7 @@ const CodeCentral = () => {
     }
     ,[])
     return (
-        <div className="h-[70vh] border-2 border-white w-full">
+        <div className="h-[70vh] border-2 border-white w-full flex flex-row">
             <div className="w-1/5 bg-green-500 h-full overflow-hidden overflow-y-scroll">
                 {
                     springBootFiles.length > 0 && springBootFiles.map((springBootClass:any) => {
@@ -42,6 +42,17 @@ const CodeCentral = () => {
                     })
 
                 }
+            </div>
+            <div className="w-1/2 bg-blue-200 h-full">
+
+            </div>
+            <div className="w-[30%] bg-red-200 h-full flex flex-col">
+                <div className="w-full h-4/5 bg-yellow-200">
+
+                </div>
+                <div className="w-full h-1/5 bg-purple-200">
+                    
+                </div>
             </div>
         </div>
     );
