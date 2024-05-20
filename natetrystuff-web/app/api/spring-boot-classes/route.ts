96ec3f4ad35b7664201509b09a559730c3bf623d
Nathan <npieraut@live.fr>
2024-05-19 21:38:36 -0400

    controller

--- natetrystuff-web/app/api/spring-boot-classes/route.ts
+++ natetrystuff-web/app/api/spring-boot-classes/route.ts
@@ -1,10 +1,32 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
-    const res = await fetch('http://localhost:1234/spring-boot-classes', {
+    const res = await fetch(`${process.env.CODE_HELPER_URL}/spring-boot-classes`, {
         headers: {
           'Content-Type': 'application/json',
+            'Cache-Control': 'no-store'
         },
+
+    });
+    const data = await res.json();
+    console.log(data)
+    return new NextResponse(JSON.stringify({ data }), {
+        status: 200,
+        headers: {
+            'Content-Type': 'application/json',
+            'Cache-Control': 'no-store'
+        },
+    });
+}
+
+export async function POST(request: NextRequest) {
+    const body = await request.json()
+    const res = await fetch(`${process.env.CODE_HELPER_URL}/spring-boot-classes`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body.meal),
     });
     const data = await res.json();
     return new NextResponse(JSON.stringify({ data }), {
--- natetrystuff-web/app/code-central.tsx
+++ natetrystuff-web/app/code-central.tsx
@@ -1,8 +1,48 @@
+import { use, useEffect, useState } from "react";
+
 const CodeCentral = () => {
+
+    const [springBootFiles, setSpringBootFiles] = useState([]);
+    const getSpringBootFiles = async () => {
+        const res = await fetch('api/spring-boot-classes', {
+            headers: {
+              'Content-Type': 'application/json',
+              'Cache-Control': 'no-store' 
+            },
+        });
+
+    const springBootFiles = await res.json();
+    setSpringBootFiles(springBootFiles.data);
+    }
+    console.log(springBootFiles)
+    useEffect(() => {
+        getSpringBootFiles();
+        
+    }
+    ,[])
     return (
         <div className="h-[70vh] border-2 border-white w-full">
+            <div className="w-1/5 bg-green-500 h-full overflow-hidden overflow-y-scroll">
+                {
+                    springBootFiles.length > 0 && springBootFiles.map((springBootClass:any) => {
+                        return (
+                            <div key={springBootClass.name}>
+                                <h1>{springBootClass.name}</h1>
+                                {
+                                    springBootClass.files.map(
+                                        (file:any, index:number) => (
+                                            <div key={index}>
+                                                <p>{file}</p>
+                                            </div>
+                                        )
+                                    )
+                                }
+                            </div>
+                        );
+                    })
 
-        
+                }
+            </div>
         </div>
     );
     }
