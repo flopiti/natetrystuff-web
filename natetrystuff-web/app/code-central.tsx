fe6365d6f13dd013670724c1e31dbfee166ebecf
Nathan <npieraut@live.fr>
2024-05-20 10:59:33 -0400

    basic of the chat

--- natetrystuff-web/app/code-central.tsx
+++ natetrystuff-web/app/code-central.tsx
@@ -20,6 +20,13 @@ const CodeCentral = () => {
         
     }
     ,[])
+
+    const [conversation, setConversation] = useState<string[]>([]);
+
+    const addToConversation = (message: string) => {
+        setConversation([...conversation, message]);
+    }
+
     return (
         <div className="h-[70vh] border-2 border-white w-full flex flex-row">
             <div className="w-1/5 bg-green-500 h-full overflow-hidden overflow-y-scroll">
@@ -48,10 +55,25 @@ const CodeCentral = () => {
             </div>
             <div className="w-[30%] bg-red-200 h-full flex flex-col">
                 <div className="w-full h-4/5 bg-yellow-200">
-
+                    {
+                        conversation.map((message, index) => {
+                            return (
+                                <div key={index} className="text-black">
+                                    <p>{message}</p>
+                                </div>
+                            );
+                        })
+                    }
                 </div>
                 <div className="w-full h-1/5 bg-purple-200">
-                    
+                    <input type="text" className="w-full h-full text-black"
+                        onKeyDown={(e) => {
+                            if (e.key === 'Enter' && e.currentTarget.value.trim() !== '') {
+                                addToConversation(e.currentTarget.value);
+                                e.currentTarget.value = '';
+                            }
+                        }}/>
+            
                 </div>
             </div>
         </div>
