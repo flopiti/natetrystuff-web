84a07ea54fa71ff5a99ae8cf0b2f5e7e2300cd93
Nathan <npieraut@live.fr>
2024-05-21 18:03:31 -0400

    remove log

--- natetrystuff-web/app/api/meals/route.ts
+++ natetrystuff-web/app/api/meals/route.ts
@@ -1,7 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(request: NextRequest) {
-    console.log('yo : ' + process.env.SPRING_BOOT_URL)
     const res = await fetch(`${process.env.SPRING_BOOT_URL}/meals`, {
         headers: {
           'Content-Type': 'application/json',
