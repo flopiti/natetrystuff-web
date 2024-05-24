a139e52b1d54d75775b60511bc188a3d8825e76d
Nathan Pieraut <72895761+flopiti@users.noreply.github.com>
2024-05-24 15:56:08 -0400

    get all filenames api route

---
+++ natetrystuff-web/app/api/get-all-filenames/route.ts
@@ -0,0 +1,20 @@
+import { NextRequest } from "next/dist/server/web/spec-extension/request";
+import { NextResponse } from "next/server";
+
+export async function GET(request: NextRequest) {
+    const { searchParams } = new URL(request.url);
+    const projectPath = searchParams.get('projectPath');
+    const res = await fetch(`${process.env.CODE_HELPER_URL}/get-all-filenames?projectPath=${projectPath}`, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+    });
+
+    const data = await res.json();
+    return new NextResponse(JSON.stringify({ data }), {
+        status: 200,
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+}
