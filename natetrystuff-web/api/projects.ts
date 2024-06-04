78f3e99ecf44ae851824f72e8dd6cfd99c184322
Nathan <npieraut@live.fr>
2024-06-04 16:23:11 -0400

    nope

--- natetrystuff-web/api/projects.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-
-const projects = ['Project 1', 'Project 2', 'Project 3'];
-
-const handler = (req: NextApiRequest, res: NextApiResponse) => {
-    res.status(200).json({ projects });
-};
-
-export default handler;
