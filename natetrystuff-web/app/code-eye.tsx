9acd66b0b77a20306137d040c875a6512d31c4e3
Nathan <npieraut@live.fr>
2024-06-04 14:58:30 -0400

    code--eye

---
+++ natetrystuff-web/app/code-eye.tsx
@@ -0,0 +1,9 @@
+const CodeEye = () => {
+    return (
+        <div style={{ border: '1px solid black', padding: '20px', width: '200px', height: '100px' }}>
+            {/* Empty box */}
+        </div>
+    );
+};
+
+export default CodeEye;
