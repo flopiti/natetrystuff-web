a0dc0ba96f84fa90184f678f86650d3b1171800c
Nathan <npieraut@live.fr>
2024-06-04 15:31:54 -0400

    build the base

--- natetrystuff-web/app/codeAnalyzer.ts
+++ natetrystuff-web/app/codeAnalyzer.ts
@@ -2,33 +2,28 @@ import { Driver, Session, driver, auth } from "neo4j-driver";
 import path from "path";
 import { Project, SyntaxKind } from "ts-morph";
 
-
 class CodeAnalyzer {
     private driver: Driver;
-    private session: Session;
 
     constructor(uri: string, user: string, password: string) {
         console.log('building')
         this.driver = driver(uri, auth.basic('neo4j', 'password'));
-        
-        this.session = this.driver.session();
     }
 
     async close() {
-        await this.session.close();
         await this.driver.close();
     }
 
-    async createProjectNode(projectName: string) {
-        await this.session.run(
+    async createProjectNode(session: Session, projectName: string) {
+        await session.run(
             "MERGE (p:Project {name: $projectName})",
             { projectName }
         );
         console.log("Created or found project node")
     }
 
-    async createFileNode(fileName: string, projectName: string) {
-        await this.session.run(
+    async createFileNode(session: Session, fileName: string, projectName: string) {
+        await session.run(
             "MATCH (p:Project {name: $projectName}) "+
             "MERGE (f:File {name: $fileName})-[:BELONGS_TO]->(p)",
             { fileName, projectName }
@@ -36,8 +31,8 @@ class CodeAnalyzer {
         console.log("Created or found file node")
     }
 
-    async createFunctionNode(functionName: string, fileName: string) {
-        await this.session.run(
+    async createFunctionNode(session: Session, functionName: string, fileName: string) {
+        await session.run(
             "MATCH (f:File {name: $fileName}) " +
             "MERGE (func:Function {name: $functionName})-[:DEFINED_IN]->(f)",
             { fileName, functionName }
@@ -47,34 +42,25 @@ class CodeAnalyzer {
     async analyzeCode(projectPath: string) {
         const project = new Project();
         const projectName = path.basename(projectPath);
-        await this.createProjectNode(projectName);
+
+        const session = this.driver.session();
+        await this.createProjectNode(session, projectName);
+        await session.close();
 
         project.addSourceFilesAtPaths(path.join(projectPath, "**/*.ts"));
-
         const sourceFiles = project.getSourceFiles().filter(sf => !sf.getFilePath().includes("node_modules"));
-        console.log(sourceFiles.length)
-        console.log(sourceFiles.map(
-            (sf) => sf.getFilePath()
-        ))
-        // sourceFiles.forEach(
-        //     (sf) => {
-        //         sf.getFunctions().forEach(
-        //             async (func) => {
-        //                 console.log(func.getName());
-        //                 console.log(func.getStatements().map( (s) => s.getKindName()));
-        //                 await this.createFunctionNode(func.getName() || "", sf.getFilePath());
-        //             }
-        //         )
-        //     }
-        // )
+
         for (const sourceFile of sourceFiles) {
             const filePath = sourceFile.getFilePath();
-            await this.createFileNode(filePath, projectName);
-            
-            // const functions = sourceFile.getFunctions();
-            // for (const func of functions) {
-            //     await this.createFunctionNode(func.getName() || "", filePath);
-            // }
+            const fileSession = this.driver.session();
+            await this.createFileNode(fileSession, filePath, projectName);
+
+            for (const func of sourceFile.getFunctions()) {
+                const funcSession = this.driver.session();
+                await this.createFunctionNode(funcSession, func.getName() || "", filePath);
+                await funcSession.close();
+            }
+            await fileSession.close();
         }
     }
 }
