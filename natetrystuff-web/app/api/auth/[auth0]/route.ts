210e4b66df2451be68256f47deac5e067ae2d465
Nathan <npieraut@live.fr>
2024-06-05 17:30:38 -0400

    login

--- natetrystuff-web/app/api/auth/[auth0]/route.ts
+++ natetrystuff-web/app/api/auth/[auth0]/route.ts
@@ -1,3 +1,19 @@
-import { handleAuth } from '@auth0/nextjs-auth0';
+import { handleAuth, handleLogin } from '@auth0/nextjs-auth0';
 
-export const GET = handleAuth();
+export const GET = handleAuth({
+  async login(req:any, res:any) {
+    try {
+      await handleLogin(req, res, {
+        authorizationParams: {
+          scope: 'openid profile email offline_access',  // Request refresh token
+        
+        },
+        returnTo: '/api/auth/callback',
+        
+
+      });
+    } catch (error:any) {
+      res.status(error.status || 500).end(error.message);
+    }
+  }
+});
