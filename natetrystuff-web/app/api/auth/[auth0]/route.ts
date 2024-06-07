010e6f8ee3af9f1f9b365f7b49a4048390e8e230
Nathan <npieraut@live.fr>
2024-06-07 13:09:31 -0400

    score

--- natetrystuff-web/app/api/auth/[auth0]/route.ts
+++ natetrystuff-web/app/api/auth/[auth0]/route.ts
@@ -3,6 +3,5 @@ import { handleAuth, handleLogin } from '@auth0/nextjs-auth0';
 export const  GET = handleAuth({
   login: handleLogin({
     authorizationParams: {
-      audience: process.env.AUTH0_AUDIENCE, 
       scope: 'openid profile email offline_access',
     }})});
