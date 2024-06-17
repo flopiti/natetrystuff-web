0e46acd749a54f31a7535faf2668462e3d8c1beb
Nathan <npieraut@live.fr>
2024-06-17 19:39:26 -0400

    config test

--- natetrystuff-web/next.config.mjs
+++ natetrystuff-web/next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+
+        webpack: (config, { isServer, buildId, dev, webpack }) => {
+          // Fixes npm packages that depend on `fs` module
+          if (!isServer) {
+            config.resolve.fallback.fs = false;
+          }
+      
+          // Mark ts-morph dependencies as external
+          config.externals.push('@ts-morph/common');
+      
+          // Ignore missing optional dependencies for ssh2
+          config.plugins.push(new webpack.IgnorePlugin({ resourceRegExp: /^cpu-features$/ }));
+      
+          return config;
+        },
+            
+
+};
 
 export default nextConfig;
