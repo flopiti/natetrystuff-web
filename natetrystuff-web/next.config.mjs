a1953e783d12c5c0451a1d2f628936ed4e14cadf
Nathan <npieraut@live.fr>
2024-07-21 00:35:38 -0400

    poll

--- natetrystuff-web/next.config.mjs
+++ natetrystuff-web/next.config.mjs
@@ -2,16 +2,34 @@
 const nextConfig = {
   reactStrictMode: false, // Disable Strict Mode
   webpack: (config, { isServer, buildId, dev, webpack }) => {
+    // Reconstruct config to include new watchOptions with polling
+    config = {
+      ...config,
+      watchOptions: {
+        ...config.watchOptions,
+        poll: dev && !isServer ? 1000 : undefined,
+      }
+    };
+
     // Fixes npm packages that depend on `fs` module
     if (!isServer) {
-      config.resolve.fallback.fs = false;
+      config.resolve = {
+        ...config.resolve,
+        fallback: {
+          ...config.resolve.fallback,
+          fs: false
+        }
+      };
     }
 
     // Mark ts-morph dependencies as external
-    config.externals.push('@ts-morph/common');
+    config.externals = [...(config.externals || []), '@ts-morph/common'];
 
     // Ignore missing optional dependencies for ssh2
-    config.plugins.push(new webpack.IgnorePlugin({ resourceRegExp: /^cpu-features$/ }));
+    config.plugins = [
+      ...config.plugins,
+      new webpack.IgnorePlugin({ resourceRegExp: /^cpu-features$/ })
+    ];
 
     return config;
   },
