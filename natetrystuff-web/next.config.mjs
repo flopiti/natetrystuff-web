af049f93042cdacc4c4061ba64a4420664dcaa8b
Nathan <npieraut@live.fr>
2024-07-21 10:42:33 -0400

    testing different config

--- natetrystuff-web/next.config.mjs
+++ natetrystuff-web/next.config.mjs
@@ -1,35 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false, // Disable Strict Mode
-  webpack: (config, { isServer, buildId, dev, webpack }) => {
-    // Reconstruct config to include new watchOptions with polling
-    config = {
-      ...config,
-      watchOptions: {
-        ...config.watchOptions,
-        poll: dev && !isServer ? 1000 : undefined,
-      }
-    };
-
-    // Fixes npm packages that depend on `fs` module
-    if (!isServer) {
-      config.resolve = {
-        ...config.resolve,
-        fallback: {
-          ...config.resolve.fallback,
-          fs: false
-        }
+  reactStrictMode: false,
+  webpack: (config, { isServer, dev, webpack }) => {
+    if (dev && !isServer) {
+      config.watchOptions = {
+        poll: 1000,
       };
     }
+    if (!isServer) {
+      config.resolve.fallback.fs = false;
+    }
 
-    // Mark ts-morph dependencies as external
-    config.externals = [...(config.externals || []), '@ts-morph/common'];
+    config.externals.push('@ts-morph/common');
 
-    // Ignore missing optional dependencies for ssh2
-    config.plugins = [
-      ...config.plugins,
+    config.plugins.push(
       new webpack.IgnorePlugin({ resourceRegExp: /^cpu-features$/ })
-    ];
+    );
 
     return config;
   },
