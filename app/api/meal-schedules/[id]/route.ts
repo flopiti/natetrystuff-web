65a778537b8d312992f1aeee7579672b8d3e5fe8
Nathan <npieraut@live.fr>
2024-09-09 16:58:16 -0400

    api call

--- app/api/meal-schedules/[id]/route.ts
+++ app/api/meal-schedules/[id]/route.ts
@@ -2,7 +2,6 @@ import { getAccessToken } from "@auth0/nextjs-auth0";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function DELETE(request: NextRequest, { params }: { params: { id: number } }) {
-
     const token = (await getAccessToken()).accessToken;
     const res = await fetch(`${process.env.SPRING_BOOT_URL}/meal-schedules/${params.id}`, {
         method: 'DELETE',
@@ -10,6 +9,26 @@ export async function DELETE(request: NextRequest, { params }: { params: { id: n
             'Content-Type': 'application/json',
             Authorization: `Bearer ${token}`,
         },
+    });
+    const data = await res.json();
+    return new NextResponse(JSON.stringify({ data }), {
+        status: 200,
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+}
+
+export async function PUT(request: NextRequest, { params }: { params: { id: number } }) {
+    const token = (await getAccessToken()).accessToken;
+    const body = await request.json();
+    const res = await fetch(`${process.env.SPRING_BOOT_URL}/meal-schedules/${params.id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify(body),
     });
     const data = await res.json();
     return new NextResponse(JSON.stringify({ data }), {
