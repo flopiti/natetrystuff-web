042d6d65d0c61d99134febf648af40eae117bcd1
flopiti <npieraut@live.fr>
2024-10-05 22:55:40 -0400

    test

---
+++ app/api/git-diff/route.ts
@@ -0,0 +1,22 @@
+import { NextRequest } from "next/dist/server/web/spec-extension/request";
+import { NextResponse } from "next/server";
+
+export async function GET(request: NextRequest) {
+    const { searchParams } = new URL(request.url);
+    const projectName = searchParams.get('projectName'); // Extract projectName from query params
+    console.log('Project Name:', projectName); // Log the project name
+
+    const res = await fetch(`${process.env.CODE_HELPER_URL}/git-diff/${projectName}`, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+    });
+
+    const data = await res.json();
+    return new NextResponse(JSON.stringify({ data }), {
+        status: 200,
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+}
