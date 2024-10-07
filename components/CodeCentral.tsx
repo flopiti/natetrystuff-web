9a5a2d62140dd9704a5583a56b9db701a7a7e363
Nathan <npieraut@live.fr>
2024-10-07 19:02:13 -0400

    conversation hook

--- components/CodeCentral.tsx
+++ components/CodeCentral.tsx
@@ -6,13 +6,11 @@ import FileListDropdown from './FileListDropdown';
 import TerminalDisplay from './TerminalDisplay';
 import { fetchHighlightedFilesContent, getFile, getProjectFiles, getProjects, getTopLevelArrayElements, getTopLevelValues, handleFlightClick, replaceCode } from '../app/utils';
 import Chat from './Chat';
+import { useConversation } from '@/hooks/useConversation';
 
 const CodeCentral = () => {
-    const PROMPT = `You are a software engineer bot that mostly produces coding answers. Each time you talked to, if the code might have a coding solution, you shall 
-    answer with the JSON object {"answer": your textual answer as a chat bot, "files": [{fileName: name, code:code},{fileName2: name, code:code2} ] THE ENTIRE RESPONSE MUST BE JSON.
-    the code snippet that you think is the answer}. You are allowed to create new files if necessary.
-    If you return a code file, you return the same file name as the original file name exactly and EXACTLY the same code as the original code (apart from the changes you made). 
-    If the code is not a coding solution, simply do not include the property in the JSON object.`;
+
+    const{conversation, setConversation} = useConversation();
 
     const [terminals, setTerminals] = useState<{ id: number; terminalInstance: Terminal | null; ws: WebSocket | null }[]>([]);
     const [selectedTerminal, setSelectedTerminal] = useState<number | null>(null);
@@ -23,7 +21,6 @@ const CodeCentral = () => {
     const [projectFiles, setProjectFiles] = useState<string[]>([]);
     const [selectedFileName, setSelectedFileName] = useState<string>('');
     const [selectedFileContent, setSelectedFileContent] = useState<string>('');
-    const [conversation, setConversation] = useState<{ content: string, role: string, type: string }[]>([{ content: PROMPT, role: 'system', type: 'text' }]);
     const [activeTab, setActiveTab] = useState<string>('file');
     const [loading, setLoading] = useState<boolean>(false);
     const [messageStreamCompleted, setMessageStreamCompleted] = useState<boolean>(false);
