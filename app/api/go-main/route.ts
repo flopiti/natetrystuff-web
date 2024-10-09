9b409a374067d95c653d91446257da18a2c802c3
flopiti <npieraut@live.fr>
2024-10-09 10:33:04 -0400

    alo

---
+++ app/api/go-main/route.ts
@@ -0,0 +1,27 @@
+import { NextRequest } from "next/dist/server/web/spec-extension/request";
+import { NextResponse } from "next/server";
+
+export async function GET(request: NextRequest) {
+    const { searchParams } = new URL(request.url);
+    const projectName = searchParams.get('projectName'); // Extract projectName from query params
+    console.log('Project Name:', projectName); // Log the project name
+
+    const res = await fetch(`${process.env.CODE_HELPER_URL}/go-main/${projectName}`, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        cache: 'no-store'
+    });
+
+
+    const resText = await res.text();
+    console.log('Response:', resText); // Log the response as a string
+    const data = JSON.parse(resText);
+
+    return new NextResponse(JSON.stringify({ data }), {
+        status: 200,
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+}
