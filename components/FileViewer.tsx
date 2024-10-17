69203fb1b7c4931dab09faef53f63ad38d91a174
Nathan <npieraut@live.fr>
2024-10-17 11:03:34 -0400

    remove line

--- components/FileViewer.tsx
+++ components/FileViewer.tsx
@@ -1,7 +1,7 @@
 import { unescapeString } from '@/app/utils';
 import { RootState } from '@/store';
 import { diffLines } from 'diff';
-import React, { useState } from 'react';
+import React, { Fragment, useState } from 'react';
 import { useSelector } from 'react-redux';
 
 interface FileViewerProps {
@@ -23,12 +23,16 @@ const FileViewer: React.FC<FileViewerProps> = ({
   replaceCode,
   setSelectedChatCode
 }) => {
+  console.log(selectedFileContent);
+
+  console.log(selectedChatCode);
   const loading = useSelector((state: RootState) => state.Messages.loading);
   const [errorMessage, setErrorMessage] = useState('');
   const [successMessage, setSuccessMessage] = useState('');
-  let linesOfSelectedChatCode = [];
+
+  let linesOfSelectedChatCode: any[] = [];
   const linesOfSelectedFileContent = selectedFileContent.split('\n');
-  let displayLines: string | number | bigint | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | Promise<React.AwaitedReactNode> | React.JSX.Element[] | null | undefined = [];  
+  let displayLines: any = [];  
   if (selectedChatCode) {
     linesOfSelectedChatCode = selectedChatCode.split('\n');
     let addedLines = [];
@@ -36,28 +40,36 @@ const FileViewer: React.FC<FileViewerProps> = ({
     let untouchedLines = [];
     let lineNumber = 1;
 
-    for (let i = 0; i < linesOfSelectedFileContent.length; i++) {
-      if (!linesOfSelectedChatCode.includes(linesOfSelectedFileContent[i])) {
-        addedLines.push({ lineNumber: lineNumber, line: linesOfSelectedFileContent[i] });
+    for (let i = 0; i < linesOfSelectedChatCode.length; i++) {
+      if (!linesOfSelectedFileContent.includes(linesOfSelectedChatCode[i])) {
+        addedLines.push({ lineNumber: lineNumber, line: linesOfSelectedChatCode[i] });
       } else {
-        untouchedLines.push({ lineNumber: lineNumber, line: linesOfSelectedFileContent[i] });
+        untouchedLines.push({ lineNumber: lineNumber, line: linesOfSelectedChatCode[i] });
       }
       lineNumber++;
     }
-
-    for (let i = 0; i < linesOfSelectedChatCode.length; i++) {
-      if (!linesOfSelectedFileContent.includes(linesOfSelectedChatCode[i])) {
-        removedLines.push({ lineNumber: i + 1, line: linesOfSelectedChatCode[i] });
+    for (let i = 0; i < linesOfSelectedFileContent.length; i++) {
+      if (!linesOfSelectedChatCode.includes(linesOfSelectedFileContent[i])) {
+        removedLines.push({ lineNumber: i + 1, line: linesOfSelectedFileContent[i] });
       }
+    }
+    const removeLine = (lineNumber: number, line: string) => {
+      const newCode = linesOfSelectedChatCode.filter((_, index) => index !== lineNumber - 1);
+      setSelectedChatCode(newCode.join('\n'));      
     }
 
     const combinedLines = [...untouchedLines, ...addedLines].sort((a, b) => a.lineNumber - b.lineNumber);
 
     displayLines = combinedLines.map(({ lineNumber, line }) => (
-      <div key={lineNumber} style={{ backgroundColor: addedLines.some(added => added.lineNumber === lineNumber) ? 'lightgreen' : 'transparent' }}>
-        <span className="text-gray-500">{lineNumber}: </span>
-        {line}
-      </div>
+      <Fragment key={lineNumber}>
+        <div style={{ backgroundColor: addedLines.some(added => added.lineNumber === lineNumber) ? 'lightgreen' : 'transparent' }}>
+          <span className="text-gray-500">{lineNumber}: </span>
+          {line}
+        </div>
+        {addedLines.some(added => added.lineNumber === lineNumber) && (
+          <RemoveButton removeLine={removeLine} line={line} lineNumber={lineNumber} />
+        )}
+      </Fragment>
     ));
     
     console.log('Removed Lines: ', removedLines);
@@ -126,7 +138,7 @@ const FileViewer: React.FC<FileViewerProps> = ({
         )}
         {!loading && activeTab === 'chat' && selectedChatCode && (
           <div className='h-full inline-block'>
-            <pre>
+            <pre key={selectedChatCode}>
               {displayLines}
             </pre>
           </div>
@@ -145,11 +157,11 @@ const AddButton: React.FC<any> = ({ lineNumber, value, file, updateFile }) => {
   </button>
 }
 
-const RemoveButton: React.FC<any> = ({ lineNumber, number, file, updateFile }) => {
+const RemoveButton: React.FC<any> = ({line, lineNumber, removeLine}) => {
   return <button
     key={lineNumber}
     className="bg-blue-500 text-white p-2"
-    onClick={() => removeLine(lineNumber, number, file, updateFile)}>
+    onClick={() => removeLine(lineNumber, line)}>
     Remove code {lineNumber}
   </button>
 }
@@ -158,12 +170,5 @@ const addLine = (lineToAdd: string, lineNumber: number, file: string, updateFile
   const newCode = file.split('\n').filter((line, index, arr) => index < arr.length - 1);
   newCode.splice(lineNumber, 0, lineToAdd);
   updateFile(newCode.join('\n'));
-};
-
-const removeLine = (lineToRemove: number, length: number, file: string, updateFile: any) => {
-  const newCode = file.split('\n');
-  newCode.splice(lineToRemove, length);
-  updateFile(newCode.join('\n'));
-};
-
+}
 export default FileViewer;
