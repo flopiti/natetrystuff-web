f9ecc738c749ad8c45e4f3f7fa8414c991ffdf59
nathan <npieraut@live.fr>
2024-10-17 22:09:51 -0400

    Refactor SystemDashboard component to update style and rename variables

--- components/SystemDashboard.tsx
+++ components/SystemDashboard.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 
 const files = [
-    { id: 1, name: 'File 1', DESC: 0, FEAT: 5 },
-    { id: 2, name: 'File 2', DESC: 1, FEAT: 3 },
-    { id: 3, name: 'File 3', DESC: 0, FEAT: 7 },
+    { id: 1, name: 'File 1', desc: 0, feat: 5 },
+    { id: 2, name: 'File 2', desc: 1, feat: 3 },
+    { id: 3, name: 'File 3', desc: 0, feat: 7 },
 ];
 
 const SystemDashboard = () => {
     return (
         <div className="flex justify-around p-4 bg-gray-100">
             {files.map(file => (
-                <div key={file.id} className="file-item flex flex-col items-center p-2 border rounded shadow-md bg-white">
-                    <div className="font-bold">{file.name}</div>
-                    <div>DESC: {file.DESC}</div>
-                    <div>FEAT: {file.FEAT}</div>
+                <div key={file.id} className="file-item bg-blue-500 text-white p-4 m-2">
+                    <h3>{file.name}</h3>
+                    <p>DESC: {file.desc}</p>
+                    <p>FEAT: {file.feat}</p>
                 </div>
             ))}
         </div>
