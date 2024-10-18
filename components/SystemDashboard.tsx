ce93ae81e8cc7ed6bd30908b0428f8ee8065209c
nathan <npieraut@live.fr>
2024-10-17 22:09:02 -0400

    Enhance SystemDashboard: Add DESC and FEAT attributes to file data, improve UI styling

--- components/SystemDashboard.tsx
+++ components/SystemDashboard.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 
 const files = [
-    { id: 1, name: 'File 1' },
-    { id: 2, name: 'File 2' },
-    { id: 3, name: 'File 3' },
+    { id: 1, name: 'File 1', DESC: 0, FEAT: 5 },
+    { id: 2, name: 'File 2', DESC: 1, FEAT: 3 },
+    { id: 3, name: 'File 3', DESC: 0, FEAT: 7 },
 ];
 
 const SystemDashboard = () => {
     return (
-        <div className="flex justify-around p-4">
+        <div className="flex justify-around p-4 bg-gray-100">
             {files.map(file => (
-                <div key={file.id} className="file-item">
-                    {file.name}
+                <div key={file.id} className="file-item flex flex-col items-center p-2 border rounded shadow-md bg-white">
+                    <div className="font-bold">{file.name}</div>
+                    <div>DESC: {file.DESC}</div>
+                    <div>FEAT: {file.FEAT}</div>
                 </div>
             ))}
         </div>
