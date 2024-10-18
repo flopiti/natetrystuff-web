6e4b77feb1c7ec9041cda3fc9eafb7db88782162
nathan <npieraut@live.fr>
2024-10-17 22:10:19 -0400

    Refactor SystemDashboard component: Change file attribute names to uppercase, update styling and layout.

--- components/SystemDashboard.tsx
+++ components/SystemDashboard.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 
 const files = [
-    { id: 1, name: 'File 1', desc: 0, feat: 5 },
-    { id: 2, name: 'File 2', desc: 1, feat: 3 },
-    { id: 3, name: 'File 3', desc: 0, feat: 7 },
+    { id: 1, name: 'File 1', DESC: 0, FEAT: 3 },
+    { id: 2, name: 'File 2', DESC: 1, FEAT: 5 },
+    { id: 3, name: 'File 3', DESC: 0, FEAT: 2 },
 ];
 
 const SystemDashboard = () => {
     return (
-        <div className="flex justify-around p-4 bg-gray-100">
+        <div className="flex flex-col p-4">
             {files.map(file => (
-                <div key={file.id} className="file-item bg-blue-500 text-white p-4 m-2">
-                    <h3>{file.name}</h3>
-                    <p>DESC: {file.desc}</p>
-                    <p>FEAT: {file.feat}</p>
+                <div key={file.id} className="file-item bg-gray-100 p-2 m-2 rounded-lg shadow-md">
+                    <h2 className="text-black font-semibold">{file.name}</h2>
+                    <p className="text-black">DESC: {file.DESC}</p>
+                    <p className="text-black">FEAT: {file.FEAT}</p>
                 </div>
             ))}
         </div>
