f3fc1205d0e1f5e570208d347f0818364bba3d33
nathan <npieraut@live.fr>
2024-10-22 21:49:38 -0400

    Add db.changelog-2.2.xml to the master changelog

---
+++ app/api/process/route.ts
@@ -0,0 +1,44 @@
+// DESC: This file handles POST and GET requests to interact with the '/process' endpoint in a Spring Boot application, leveraging Auth0 for authentication.
+import { getAccessToken } from '@auth0/nextjs-auth0';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function POST(request: NextRequest) {
+    const token = (await getAccessToken()).accessToken;
+    const body = await request.json();
+    const res = await fetch(`${process.env.SPRING_BOOT_URL}/process`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify(body),
+    });
+    const data = await res.json();
+    return new NextResponse(JSON.stringify({ data }), {
+        status: res.status,
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+}
+
+export async function GET(request: NextRequest) {
+    const token = (await getAccessToken()).accessToken;
+
+    const apiUrl = `${process.env.SPRING_BOOT_URL}/process`;
+    const res = await fetch(apiUrl, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        }
+    });
+
+    const data = await res.json();
+    return new NextResponse(JSON.stringify({ data }), {
+        status: res.status,
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+}
