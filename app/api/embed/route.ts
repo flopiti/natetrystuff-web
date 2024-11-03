8ecbdb2b7db961d4c8167c5749dec9f796c67e32
Nathan <npieraut@live.fr>
2024-11-03 12:30:14 -0500

    route

--- app/api/embed/route.ts
+++ app/api/embed/route.ts
@@ -1,14 +1,53 @@
 //DESC: This file handles POST requests to embed data in Pinecone index.
 
 import { NextRequest, NextResponse } from 'next/server';
+import { Pinecone } from '@pinecone-database/pinecone';
+import OpenAI from "openai";
 
-const pc = new Pinecone({ apiKey: '24353792-dce7-4d9b-820f-9d30202e3669' });
-const index = pc.index('quickstart');
 
 export async function POST(request: NextRequest) {
-    const { vectors } = await request.json();
+    const x = await request.json();
+    console.log('x it')
+    console.log(x)
+    // console.log(vectors)
+    // console.log(selectedFileName)
+    // console.log(selectedFileContent)
+
+
+    const openai = new OpenAI({
+        apiKey: process.env.OPEN_AI_API_KEY
+    });
+    const embedding = await openai.embeddings.create({
+        model: "text-embedding-3-small",
+        input: x.toEmbed,
+        encoding_format: "float",
+      });
+
+      console.log('embedding')
+      console.log(embedding.data[0].embedding)
+
+      console.log(embedding.data.map((value: any) => parseFloat(value)))
+
+    const pc = new Pinecone({
+        apiKey: '24353792-dce7-4d9b-820f-9d30202e3669'
+    });
+    const index = pc.index('codebase');
+
+    console.log(embedding.data.map((value: any) => parseFloat(value)))
     try {
-        await index.namespace('ns1').upsert(vectors);
+        await index.namespace('ns1').upsert([
+            {
+                id: x.id, // Assuming x has an id property
+                values: embedding.data[0].embedding
+            }
+        ]);
+    } catch (error) {
+        console.error('Error upserting vectors:', error);
+        throw error;
+    }
+    ;
+
+    try {
         return new NextResponse(JSON.stringify({ message: 'Vectors upserted successfully' }), {
             status: 200,
             headers: {
