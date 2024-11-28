e25e8971afa5e08a288c01ead5d5e308f651edd6
Nathan <npieraut@live.fr>
2024-11-28 07:51:06 -0500

    style

--- components/DevBox.tsx
+++ components/DevBox.tsx
@@ -10,14 +10,14 @@ interface DevBoxProps {
     stop: () => void;
 }
 
-const DevBox = ({  name,isRunning, start, stop }:DevBoxProps) => {
+const DevBox = ({ name, isRunning, start, stop }: DevBoxProps) => {
     const [isHovered, setIsHovered] = useState<boolean>(false);
-    
+
     return (
-        <div  
+        <div
             id='api-box'
-            className="flex flex-row items-center border-2 border-white"
-            onMouseEnter={() => setIsHovered(true)} 
+            className="relative flex flex-row items-center border-2 border-white"
+            onMouseEnter={() => setIsHovered(true)}
             onMouseLeave={() => setIsHovered(false)}
         >
             {isRunning !== null && (
@@ -26,9 +26,9 @@ const DevBox = ({  name,isRunning, start, stop }:DevBoxProps) => {
                     animate={isRunning ? { rotate: 360 } : {}}
                     transition={isRunning ? { repeat: Infinity, duration: 4, ease: "linear" } : {}}
                 >
-                    <FontAwesomeIcon 
-                        size="xl" 
-                        icon={isRunning ? faArrowsRotate : faTimesCircle} 
+                    <FontAwesomeIcon
+                        size="xl"
+                        icon={isRunning ? faArrowsRotate : faTimesCircle}
                     />
                 </motion.div>
             )}
@@ -37,28 +37,28 @@ const DevBox = ({  name,isRunning, start, stop }:DevBoxProps) => {
             </div>
             {isHovered && (
                 <motion.div
-                    className="w-full h-full flex items-center justify-center bg-opacity-50 bg-black"
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    transition={{ duration: 0.3 }}
+                    className="absolute inset-0 flex items-center justify-center bg-opacity-75 bg-black"
+                    initial={{ opacity: 0, scale: 0.8 }}
+                    animate={{ opacity: 1, scale: 1 }}
+                    transition={{ duration: 0.3, type: 'spring', stiffness: 100 }}
                 >
                     {isRunning ? (
-                        <motion.button 
-                            className="bg-red-500 text-whiterounded w-full"
+                        <motion.button
+                            className="bg-red-500 text-white p-2 mx-2 rounded"
                             initial={{ scale: 0 }}
                             animate={{ scale: 1 }}
                             transition={{ type: 'spring', stiffness: 120 }}
-                            onClick={() => stop}
+                            onClick={stop}
                         >
                             Stop
                         </motion.button>
                     ) : (
-                        <motion.button 
-                            className="bg-green-500 text-white p-2 mx-2 rounded w-full"
+                        <motion.button
+                            className="bg-green-500 text-white p-2 mx-2 rounded"
                             initial={{ scale: 0 }}
                             animate={{ scale: 1 }}
                             transition={{ type: 'spring', stiffness: 120 }}
-                            onClick={() => start()} 
+                            onClick={start}
                         >
                             Start
                         </motion.button>
