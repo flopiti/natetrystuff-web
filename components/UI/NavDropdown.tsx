bbd383c5864ab7a2709e157379d29670ecc7b8b4
Nathan <npieraut@live.fr>
2025-01-04 23:32:29 -0500

    durations

--- components/UI/NavDropdown.tsx
+++ components/UI/NavDropdown.tsx
@@ -3,59 +3,51 @@ import { motion, AnimatePresence } from 'framer-motion';
 
 const NavDropdown = () => {
   const [hovered, setHovered] = useState(false);
-  
   const items = ["sched", "meals", "code", "chess", "todo", "text"];
 
-  // Variants for the container to handle staggered children animations
-  const containerVariants = {
-    initial: {},
-    hovered: { 
+  const dropdownVariants = {
+    hidden: { height: 0, opacity: 0 },
+    visible: { 
+      height: 'auto', 
+      opacity: 1, 
       transition: { 
-        staggerChildren: 0.2, // Stagger each child by 0.2 seconds
-      },
+        staggerChildren: 0.2, 
+        duration: 0.6 // Change this value
+      } 
     },
   };
-
-  // Variants for each dropdown item
+  
   const itemVariants = {
     hidden: { opacity: 0, y: -10 },
     visible: { 
       opacity: 1, 
       y: 0,
-      transition: { duration: 0.5 },
+      transition: { duration: 1} // Change this value
     },
   };
-
+  
   return (
-    <motion.div
-      className="py-4 m-5 border-black border-[1px] font-Orbitron text-xs font-bold absolute top-0 right-0 w-[5rem] text-center overflow-hidden"
+    <div
+      className="py-4 m-5 border-black border-[1px] font-Orbitron text-xs font-bold absolute top-0 right-0 w-[5rem] text-center"
       onMouseEnter={() => setHovered(true)}
       onMouseLeave={() => setHovered(false)}
-      variants={containerVariants}
-      initial="initial"
-      animate={hovered ? "hovered" : "initial"}
-      layout // Enables automatic layout animations
     >
       <div>Nav</div>
-      
-      {/* AnimatePresence handles the mounting and unmounting animations */}
       <AnimatePresence>
         {hovered && (
           <motion.div
-            className='my-2'
-            initial={{ height: 0, opacity: 0 }}
-            animate={{ height: 'auto', opacity: 1 }}
-            exit={{ height: 0, opacity: 0 }}
-            transition={{ duration: 0.5 }}
+            className='mt-2 flex flex-col items-center'
+            initial="hidden"
+            animate="visible"
+            exit="hidden"
+            variants={dropdownVariants}
+            style={{ overflow: 'hidden' }}
           >
             {items.map((item, index) => (
               <motion.button
                 className='py-1 block text-center w-full'
                 key={index}
                 variants={itemVariants}
-                initial="hidden"
-                animate="visible"
-                exit="hidden"
               >
                 {item}
               </motion.button>
@@ -63,7 +55,7 @@ const NavDropdown = () => {
           </motion.div>
         )}
       </AnimatePresence>
-    </motion.div>
+    </div>
   );
 };
 
