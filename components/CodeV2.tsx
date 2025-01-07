dcaab368efe4664242f8c37de4db192431730705
Nathan <npieraut@live.fr>
2025-01-07 08:02:00 -0500

    base comp

--- components/CodeV2.tsx
+++ components/CodeV2.tsx
@@ -1,4 +1,33 @@
+import { ProjectPath } from "@/interfaces/project";
+import { getAllProjects } from "@/services/gitService";
+import { fetchProjectPaths } from "@/services/projectPathService";
+import { Project } from "@/types/project";
+import { useEffect, useState } from "react";
+
 const CodeV2 = () => {
+
+    //state
+    const[AllProjects, setAllProjects] = useState<Project[]>([]);
+    const [projectPath, setProjectPath] = useState<string | null>(null);
+
+
+    //fetch useEffects
+    //1. Fetch project paths
+    useEffect(() => {
+        fetchProjectPaths().then((data: ProjectPath[]) => {
+            setProjectPath(data[0].path);
+        });
+    }, []);
+
+    //2. Fetch all projects
+    useEffect(() => {
+        if (!projectPath) return;
+        getAllProjects(projectPath).then((projects:Project[]) => {
+            setAllProjects(projects);
+        });
+    }, [projectPath]);
+
+
     return (
         <div>
             <h1>CodeV2</h1>
