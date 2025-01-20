b728ecaabd106d4cfeef6efd9572b955b9696d32
Nathan <npieraut@live.fr>
2025-01-19 23:09:21 -0500

    show embedded

--- components/UI/CustomButton.tsx
+++ components/UI/CustomButton.tsx
@@ -1,12 +1,11 @@
 import { ReactNode } from "react";
 
- 
 interface ButtonProps {
     onClick: () => void;
     children: ReactNode;
 }
 
-const CustomButton =  (props: ButtonProps) => {
-    return <button className="px-12 py-4 border-black border-[1px] font-Orbitron text-xs font-bold " {...props} />;
-    }
+const CustomButton =  ({onClick, children, className = ""}: ButtonProps & { className?: string }) => {
+    return <button className={`px-8 py-2 border-black border-[1px] font-Orbitron text-xs font-bold ${className}`} onClick={onClick}>{children}</button>
+}
 export default CustomButton;
